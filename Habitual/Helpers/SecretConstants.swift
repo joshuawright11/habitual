25a0b1a8db8c9e966986be132a71e7d4d6b5cb8f
Josh Wright <joshuawright11@gmail.com>
2016-04-29 00:33:26 -0500

    Closes #80

---
+++ Habitual/Helpers/SecretConstants.swift
@@ -0,0 +1,18 @@
+//
+//  SecretConstants.swift
+//  Habitual
+//
+//  Created by Josh Wright on 10/14/15.
+//  Copyright © 2015 Josh Wright. All rights reserved.
+//
+
+import Foundation
+
+// -TODO: Needs refactoring/documentation
+
+struct SecretConstants {
+    
+}
+
+let kParseApplicationId = "1qF0PH01jWWL6UeRTTBQAqE81pg2dOd1m1nzRcmu"
+let kParseClientKey = "MqtooBZ1p1ooXsFEw7kULVBEC7Q5JSpDyCdViw48"
